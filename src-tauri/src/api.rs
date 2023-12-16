050b7fb749aa0f311f806e9a661ceb2e1e4262c8
Aditya <aditya26sg@gmail.com>
2023-12-16 23:33:01 +0530

    added api headers

---
+++ src-tauri/src/api.rs
@@ -0,0 +1,17 @@
+use reqwest::header::{ACCEPT, AUTHORIZATION, HeaderMap, HeaderValue, USER_AGENT};
+use serde::Serialize;
+use crate::models::{APIResult, URL};
+
+fn construct_headers(token: Option<&str>) -> HeaderMap {
+    let mut headers = HeaderMap::new();
+
+    headers.insert(ACCEPT, HeaderValue::from_static("application/vnd.github+json"));
+    headers.insert(USER_AGENT, HeaderValue::from_static("Dashhub demo"));
+
+    if let Some(token) = token {
+        let token = format!("Bearer {token}");
+        let header_value = HeaderValue::from_str(token.as_str()).expect("Could not generate header from value");
+        headers.insert(AUTHORIZATION, header_value);
+    }
+    headers
+}
