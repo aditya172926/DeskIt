6bd4157674d733bded51a609b886f4cc32378d3a
Aditya <aditya26sg@gmail.com>
2023-12-16 17:22:00 +0530

    error

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,2 +1,27 @@
 use std::fmt::{Display, Formatter};
 use reqwest::StatusCode;
+
+#[derive(Debug)]
+pub struct TauriError {
+    pub message: &'static str
+}
+
+impl From<reqwest::Error> for TauriError {
+    fn from (error: reqwest::Error) -> Self {
+        let error_message = match error.status().unwrap() {
+            StatusCode::FORBIDDEN => "This endpoint requires a token",
+            StatusCode::BAD_REQUEST => "There was a problem with your request",
+            _ => "Something went wrong handling this request"
+        };
+        TauriError {
+            message: error_message
+        }
+    }
+}
+
+impl serde::Serialize for TauriError {
+    fn serialize<S>(&self, serialize:S) -> Result<S::Ok, S::Error> where S: serde::ser::Serializer,
+    {
+        serialize.serialize_str(self.to_string().as_ref())
+    }
+}
