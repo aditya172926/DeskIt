c8c1d5d0e5cc118203c30bb7be4e2002c623fa90
Aditya <aditya26sg@gmail.com>
2023-12-18 20:29:43 +0530

    repo details

--- src/components/repository/RepositoryDetails.tsx
+++ src/components/repository/RepositoryDetails.tsx
@@ -27,15 +27,16 @@ const RepositoryDetails = ({ repository, token = null }: Props) => {
       );
 
       const contributors: GithubUser[] = await invoke(
-        "get_users_associated_with_repository", {
-            url: repository.contributors_url,
-            token
+        "get_users_associated_with_repository",
+        {
+          url: repository.contributors_url,
+          token,
         }
       );
 
       const commits: Commit[] = await invoke("get_commits_to_repository", {
         url: repository.commits_url.replace(/{\/[a-z]*}/, ""),
-        token
+        token,
       });
 
       setCommits(commits);
@@ -47,16 +48,39 @@ const RepositoryDetails = ({ repository, token = null }: Props) => {
   }, [repository]);
 
   return (
-    <Card title={repository.name} bordered={false} style={{margin: "1%"}}>
-        {repository.description}
+    <Card title={repository.name} bordered={false} style={{ margin: "1%" }}>
+      {repository.description}
+      <Divider />
+
+      <Spin tip="Loading" spinning={isLoading}>
+        <Typography.Title level={5} style={{ margin: 10 }}>
+          Contributors
+        </Typography.Title>
+        <GithubUserGrid users={contributors} />
         <Divider />
 
-        <Spin tip="Loading" spinning={isLoading}>
-            <Typography.Title level={5} style={{margin: 10}}>
-                Contributors
-            </Typography.Title>
-            <GithubUserGrid users={contributors} />
-        </Spin>
+        <Typography.Title level={5} style={{ marginBottom: 15 }}>
+          Stargazers
+        </Typography.Title>
+        <GithubUserGrid users={stargazers} />
+        <Divider />
+
+        <Typography.Title level={5} style={{ marginBottom: 15 }}>
+          Commits
+        </Typography.Title>
+        <Timeline mode="alternate">
+          {commits.map((commit, index) => {
+            if (commit.commit) {
+              return (
+                <Timeline.Item key={index}>
+                  {commit.commit?.message}
+                </Timeline.Item>
+              );
+            }
+            return null;
+          })}
+        </Timeline>
+      </Spin>
     </Card>
-  )
+  );
 };
