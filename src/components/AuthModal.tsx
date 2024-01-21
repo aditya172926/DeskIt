eb3c105e420ed050e7d35dd330b8fac5c1616a2a
Aditya Singh <aditya26sg@gmail.com>
2024-01-21 19:01:24 +0530

    fixed authflow modal with rust cmd

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,5 +1,5 @@
 import { invoke } from "@tauri-apps/api/tauri";
-import { Avatar, Button, Col, Form, Modal, Row, Typography } from "antd";
+import { Avatar, Button, Col, Modal, Row, Typography } from "antd";
 
 interface Props {
   shouldShowModal: boolean;
@@ -9,13 +9,19 @@ interface Props {
 
 const AuthModal = ({ shouldShowModal, onSubmit, onCancel }: Props) => {
   const { Title } = Typography;
+
   const newWindow = async () => {
     try {
-      await invoke("generate_new_window", {
-        url: "https://github.com/login/device",
-        label: "Authentication",
-        title: "GitHub Auth",
-      });
+      const response = await invoke("call_api_method", {method: "POST", url: "https://github.com/login/device/code?client_id=", data: {
+        "key1": "value1",
+        "key2": "value2"
+      }})
+    console.log("Fetch response", response);
+      // await invoke("generate_new_window", {
+      //   url: "https://github.com/login/device",
+      //   label: "Authentication",
+      //   title: "GitHub Auth",
+      // });
     } catch (error) {
       console.log("Error", error);
     }
@@ -23,12 +29,12 @@ const AuthModal = ({ shouldShowModal, onSubmit, onCancel }: Props) => {
 
   return (
     <Modal centered footer={null} open={shouldShowModal} onCancel={onCancel}>
-      <Row>
+      <Row justify="center">
         <Col>
           <Title level={4}>Authenticate using Github</Title>
         </Col>
       </Row>
-      <Row style={{margin: "5%"}}>
+      <Row justify="center" style={{margin: "5%"}}>
         <Col>
           <Avatar shape="square" size={50} src="../../app-icon.png" /> -------{" "}
           <Avatar shape="square" size={50} src="../../github-mark.png" />
